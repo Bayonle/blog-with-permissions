a4a6e8b2a0645aeff60ac870db5c732e836d21ac
Bayonle Amzat <zedcrest@Zedcrests-MacBook-Pro.local>
2020-05-15 11:18:29 +0100

    📦NEW: Project setup

---
+++ components/Post.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="w-full mb-10">
+        <h1 class="text-xl font-bold text-black">{{post.title}}</h1>
+        <p>{{post.body}}</p>
+        <div class="mt-5">
+            <!-- <span v-html="status"></span> -->
+            <span class=" bg-orange-100 text-yellow-800 px-1 py-1 text-sm">Pending review</span>
+        </div>
+        <div class="flex items-center">
+
+        </div>
+        <button>Review</button>
+        <button>Approve</button>
+        <button>Publish</button>
+    </div>
+</template>
+
+<script>
+export default {
+    computed:{
+        status(){
+            if(this.post.status === 'pending_review'){
+                return `spa`
+            }
+        }
+    },
+    props:{
+        post:{
+            type: Object,
+            default: () => {}
+        }
+    }
+}
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="w-2/3 mx-auto px-48 py-16">
+        <post v-for="(post, index) in posts" :key="index" :post="post" />
+    </div>
+</template>
+
+<script>
+import posts from '@/data/posts'
+import comments from '@/data/comments'
+import Post from '@/components/Post'
+export default {
+    data(){
+        return {
+            posts,
+            comments
+        }
+    },
+    components:{
+        Post
+    }
+
+}
+</script>
